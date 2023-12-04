9c156895c1a5af57f298517aa29d0aa946bfe61c
dimitar popchev <dpopchev@gmail.com>
2023-12-04 21:12:31 +0200

    finish day1 task1

--- src/advent2023/day1.py
+++ src/advent2023/day1.py
@@ -1,7 +1,11 @@
-from typing import Iterable, Optional
-def find_digit(line: Iterable[str]) -> Optional[str]:
-    return next(filter(lambda _: _.isdigit(), line), None)
+from typing import Iterable
+from dataclasses import dataclass
 
+def find_digit(line: Iterable[str]) -> str:
+    return next(filter(lambda _: _.isdigit(), line))
 
-def find_calibration_value(line: str) -> Optional[str]:
+def find_calibration_value(line: str) -> str:
     return f"{find_digit(line)}{find_digit(reversed(line))}"
+
+def find_calibration_value_sum(lines: Iterable[str]) -> int:
+    return sum(map(int, (find_calibration_value(l) for l in lines)))
--- tests/day1/test_finding_calibration_value.py
+++ tests/day1/test_finding_calibration_value.py
@@ -1,12 +1,13 @@
+from typing import Iterable
 import pytest
-from advent2023.day1 import find_digit
+from advent2023.day1 import find_calibration_value_sum, find_digit
 from advent2023.day1 import find_calibration_value
 
-SCENARIOS = (
-    ['1abc2', '12',],
-    ['pqr3stu8vwx', '38'],
-    ['a1b2c3d4e5f', '15'],
-    ['treb7uchet', '77']
+SCENARIOS:Iterable[tuple[str, str]] = (
+    ('1abc2', '12',),
+    ('pqr3stu8vwx', '38'),
+    ('a1b2c3d4e5f', '15'),
+    ('treb7uchet', '77')
 )
 
 @pytest.mark.parametrize('tcase, expected', SCENARIOS, ids=(s[0] for s in SCENARIOS))
@@ -16,3 +17,8 @@ def test_finding_frist_calibration_digit(tcase, expected):
 @pytest.mark.parametrize('tcase, expected', SCENARIOS, ids=(s[0] for s in SCENARIOS))
 def test_finding_calibration_values(tcase, expected):
     assert find_calibration_value(tcase) == expected
+
+@pytest.mark.parametrize('scenarios', [SCENARIOS])
+def test_calibration_value_sum(scenarios):
+    actual = find_calibration_value_sum((s for s, _ in scenarios))
+    assert actual == sum(map(int, (s for _, s in scenarios)))

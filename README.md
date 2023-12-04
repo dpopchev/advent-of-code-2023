cca573b4b9eecfa99c1673df3f558db8bf634ae5
dimitar popchev <dpopchev@gmail.com>
2023-12-04 14:40:06 +0200

    template project

---
+++ README.md
@@ -0,0 +1,9 @@
+# advent2023
+
+Excellent package with much to offer
+
+## Quickstart
+```
+git clone URL
+pyseed=/path/python make development
+```
---
+++ tests/day1/test_trebuchet.py
@@ -0,0 +1,4 @@
+import pytest
+from advent2023 import sample
+def test_scenario_1(): assert sample() == 0
+def test_scenario_2(): assert not sample() != 0

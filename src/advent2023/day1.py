402bd6837f4c6d292897091c67c98c538e973228
dimitar popchev <dpopchev@gmail.com>
2023-12-04 16:56:17 +0200

    solution attempt

--- src/advent2023/day1.py
+++ src/advent2023/day1.py
@@ -1,1 +1,7 @@
-def sample(): return 0
+from typing import Iterable, Optional
+def find_digit(line: Iterable[str]) -> Optional[str]:
+    return next(filter(lambda _: _.isdigit(), line), None)
+
+
+def find_calibration_value(line: str) -> Optional[str]:
+    return f"{find_digit(line)}{find_digit(reversed(line))}"
--- tests/day1/test_finding_calibration_value.py
+++ tests/day1/test_finding_calibration_value.py
@@ -1,1 +1,18 @@
+import pytest
+from advent2023.day1 import find_digit
 from advent2023.day1 import find_calibration_value
+
+SCENARIOS = (
+    ['1abc2', '12',],
+    ['pqr3stu8vwx', '38'],
+    ['a1b2c3d4e5f', '15'],
+    ['treb7uchet', '77']
+)
+
+@pytest.mark.parametrize('tcase, expected', SCENARIOS, ids=(s[0] for s in SCENARIOS))
+def test_finding_frist_calibration_digit(tcase, expected):
+    assert find_digit(tcase) == expected[0]
+
+@pytest.mark.parametrize('tcase, expected', SCENARIOS, ids=(s[0] for s in SCENARIOS))
+def test_finding_calibration_values(tcase, expected):
+    assert find_calibration_value(tcase) == expected

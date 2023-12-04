0c0aa129f96eb0f85dfbcea965000e4afb9175d5
dimitar popchev <dpopchev@gmail.com>
2023-12-04 23:43:40 +0200

    fix conrenr case with regressions

--- src/advent2023/day1.py
+++ src/advent2023/day1.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Pattern
 import re
 
 WORDED_DIGITS = {
@@ -13,14 +13,23 @@ WORDED_DIGITS = {
     'nine': '9'
 }
 
+REVERSED_WORDED_DIGITS = { k[::-1]: v for k, v in WORDED_DIGITS.items() }
+
 DIGIT_PATTERN = re.compile(f'[0-9]|{"|".join(WORDED_DIGITS.keys())}')
+REVERSED_DIGIT_PATTERN = re.compile(f'[0-9]|{"|".join(REVERSED_WORDED_DIGITS.keys())}')
+
+def get_first_match(line:str, pattern:Pattern) -> str:
+    return next(pattern.finditer(line))[0]
+
+def get_digit_value(digit:str, lookup:dict) -> str:
+    if digit in lookup:
+        return lookup[digit]
+    return digit
 
 def find_digit(line: str, is_last=False) -> str:
-    matches = (m[0] for m in DIGIT_PATTERN.finditer(line))
-    digits = [WORDED_DIGITS[m] if m in WORDED_DIGITS else m for m in matches]
     if is_last:
-        return digits[-1]
-    return digits[0]
+        return get_digit_value(get_first_match(line[::-1], REVERSED_DIGIT_PATTERN), REVERSED_WORDED_DIGITS)
+    return get_digit_value(get_first_match(line, DIGIT_PATTERN), WORDED_DIGITS)
 
 def find_calibration_value(line: str) -> str:
     return f"{find_digit(line)}{find_digit(line, is_last=True)}"
